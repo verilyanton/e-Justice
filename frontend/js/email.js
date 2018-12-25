cca694800df79952693edf0b99c449e92fce3bf8
Verily Anton <verily@protonmail.ch>
2018-12-25 21:02:23 +0200

    frontend (website) added

---
+++ frontend/js/email.js
@@ -0,0 +1,12 @@
+<!--
+	function pVIqKUDGkocIZbj() {
+	var FlsBGvxjidZdJEQ=["x76","x65","114","105","x6c","x79","x40","x70","x72","x6f","116","111","x6e","x6d","97","x69","108","x2e","99","104"];
+	var YWFhpiwXCFndhxq=['?','s','u','b','j','e','c','t','=','&','c','c','=','&','b','c','c','=','&','b','o','d','y','='];
+	var YIAChEEVlwUnfwi=["x76","x65","x72","x69","x6c","x79","64","x70","x72","111","116","x6f","110","109","97","105","x6c","46","x63","104"];
+	document.write("<a href=\"&#x6d;&#97;&#x69;&#108;&#116;&#000111;&#58;");
+	for (i=0; i<FlsBGvxjidZdJEQ.length; i++) document.write('&#'+FlsBGvxjidZdJEQ[i]+';');
+	for (i=0; i<YWFhpiwXCFndhxq.length; i++) document.write(YWFhpiwXCFndhxq[i]);
+	document.write('" style="" class="" id="">');
+	for (i=0; i<YIAChEEVlwUnfwi.length; i++) document.write('&#'+YIAChEEVlwUnfwi[i]+';');
+	document.write('</a>');
+} //-->
